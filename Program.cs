1d6eff041ce63b1a95e3e854671ae0e98e441a80
sealanguage <elaine.froehlich@gmail.com>
2019-05-11 13:33:26 -0400

    Initial check-in of module variablesAndConstants

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace variablesAndConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            Console.WriteLine(number);
+        }
+    }
+}
+
