ecf7b29c32f7eb22edde2bc337a1961c0b141e6e
sealanguage <elaine.froehlich@gmail.com>
2019-05-11 14:07:18 -0400

    all varialbes loaded and working

--- Program.cs
+++ Program.cs
@@ -7,7 +7,24 @@ namespace variablesAndConstants
         static void Main(string[] args)
         {
             byte number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+
         }
     }
 }
